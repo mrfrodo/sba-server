0bdbf5b2f8db3ca3910522e6dd57992ff6165032
Frodo <frodo@bagend.com>
2020-11-19 19:07:13 +0100

    Added the annotation

--- src/main/java/no/frodo/sba/SBAApplication.java
+++ src/main/java/no/frodo/sba/SBAApplication.java
@@ -1,8 +1,10 @@
 package no.frodo.sba;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableAdminServer
 @SpringBootApplication
 public class SBAApplication {
 
